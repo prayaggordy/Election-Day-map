25607315e8787c7487a0805985a715daa6f598fe
Prayag Gordy <prayaggordy@gmail.com>
2020-11-03 23:17:17 -0600

    fixed tables

--- scrape_md.R
+++ scrape_md.R
@@ -1,21 +1,22 @@
-easypackages::libraries("tidyverse", "rvest", "janitor")
+easypackages::libraries("tidyverse", "rvest", "janitor", "knitr", "kableExtra", "rmarkdown")
 
 reporting <- read_html("https://results.elections.maryland.gov/county_status_page_root.html") %>%
 	html_text() %>%
+	str_remove_all("\\*") %>%
 	str_split("background-color", simplify = T)
 
 reporting_split_init <- reporting[2] %>%
 	str_split("\r\n")
 
-reporting_table <- reporting_split_init[[1]][3:202]
+reporting_table <- reporting_split_init[[1]][3:177]
 
-reporting_df <- as.data.frame(matrix(reporting_table, ncol = 8, byrow = T), stringsAsFactors = F) %>%
+reporting_df <- as.data.frame(matrix(reporting_table, ncol = 7, byrow = T), stringsAsFactors = F) %>%
 	row_to_names(1) %>%
 	clean_names() %>%
 	separate(col = number_election_day_vote_center_scanners_reported1, into = c("reporting_centers", "total_centers"), sep = " of ") %>%
 	mutate_at(vars(2:4), ~parse_number(.)) %>%
 	mutate_all(~str_trim(.)) %>%
-	mutate_at(vars(5:9), ~ifelse(. == "3Local", "Local results only", .))
+	mutate_at(vars(5:ncol(.)), ~ifelse(. == "3Local", "Local results only", .))
 
 pres <- read_html("https://results.elections.maryland.gov/elections/2020/results/General/gen_detail_results_2020_4_BOT001-.html") %>%
 	html_node(xpath = '//*[@id="primary_right_col"]/div/div[2]/table') %>%
@@ -26,9 +27,9 @@ pres <- read_html("https://results.elections.maryland.gov/elections/2020/results
 	mutate(total_votes = trump + biden,
 				 trump_pct = trump/total_votes,
 				 biden_pct = biden/total_votes) %>%
-	inner_join(select(reporting_df, county, reported = percent_election_day_results_reported2), by = "county") %>%
-	select(-total_votes) %>%
-	mutate(reported = as.numeric(reported)/100)
+	# inner_join(select(reporting_df, county, reported = percent_election_day_results_reported2), by = "county") %>%
+	select(-total_votes)# %>%
+	# mutate(reported = as.numeric(reported)/100)
 
 boe_al <- read_html("https://results.elections.maryland.gov/elections/2020/results/general/gen_results_2020_4_by_county_160.html") %>%
 	html_node(xpath = '//*[@id="primary_right_col"]/div/div[2]/table') %>%
@@ -67,16 +68,6 @@ boe_d4 <- read_html("https://results.elections.maryland.gov/elections/2020/resul
 	mutate(Race = "Board of Education, At-Large") %>%
 	select(1, 2) %>%
 	mutate_all(~ paste0(., "%"))
-
-kable(boe_al) %>%
-	kable_styling(bootstrap_options = "responsive", full_width = F) %>%
-	save_kable(file = "boe_al.html", self_contained = T)
-kable(boe_d2) %>%
-	kable_styling(bootstrap_options = "responsive", full_width = F) %>%
-	save_kable(file = "boe_d2.html", self_contained = T)
-kable(boe_d4) %>%
-	kable_styling(bootstrap_options = "responsive", full_width = F) %>%
-	save_kable(file = "boe_d4.html", self_contained = T)
 
 ballot_a <- read_html("https://results.elections.maryland.gov/elections/2020/results/general/gen_qresults_2020_4_16_1.html") %>%
 	html_node(xpath = '//*[@id="primary_right_col"]/div/div[2]/table') %>%
@@ -121,17 +112,20 @@ ballot_d <- read_html("https://results.elections.maryland.gov/elections/2020/res
 ballot_questions <- rbind(ballot_a, ballot_b, ballot_c, ballot_d) %>%
 	mutate(For = paste0(For, "%"), Against = paste0(Against, "%"))
 
-kable(ballot_questions) %>%
-	kable_styling(bootstrap_options = "responsive", full_width = F) %>%
-	save_kable(file = "ballot_questions.html", self_contained = T)
-
-kable(select(reporting_df, 1:4) %>% mutate(percent_election_day_results_reported2 = paste0(percent_election_day_results_reported2, "%")), col.names = c("County", "Election day vote center scanners reported", "Total election day vote center scanners", "Election day percent reported")) %>%
-	kable_styling(bootstrap_options = "responsive", full_width = F) %>%
-	save_kable(file = "reporting.html", self_contained = T)
+# reporting_table_df <- unite(reporting_df, `Election Day Vote Center Scanners Reported`, c("reporting_centers", "total_centers"), sep = " of ") %>%
+# 	select(County = 1, 2, `Election day percent reported` = 3) %>%
+# 	mutate_at(vars(3), ~ paste0(., "%"))
 
 write_csv(pres, "pres.csv")
 write_csv(reporting_df, "reporting.csv")
+# write_csv(reporting_table_df, "reporting_table.csv")
 write_csv(boe_al, "boe_al.csv")
 write_csv(boe_d2, "boe_d2.csv")
 write_csv(boe_d4, "boe_d4.csv")
 write_csv(ballot_questions, "ballot_questions.csv")
+
+render("tables.Rmd", params = list(fn = "boe_al"), output_file = "boe_al.html")
+render("tables.Rmd", params = list(fn = "boe_d2"), output_file = "boe_d2.html")
+render("tables.Rmd", params = list(fn = "boe_d4"), output_file = "boe_d4.html")
+render("tables.Rmd", params = list(fn = "ballot_questions"), output_file = "ballot_questions.html")
+# render("tables.Rmd", params = list(fn = "reporting_table_df"), output_file = "reporting.html")
